57d4d69725b5f5037bc416c1158e6dec50cfc27e
ugurkilic81 <ugurkilic91@gmail.com>
2022-11-29 22:58:16 +0300

    Vsix Console Test Project

---
+++ Extensions/VsixFUArchitecture/ConsoleAppVsixTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppVsixTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
